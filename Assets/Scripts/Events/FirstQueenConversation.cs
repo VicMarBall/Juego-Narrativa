e82dfde279b5b71705f481504648a3b26b0f0252
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2025-04-22 19:59:38 +0200

    show change of values

--- Assets/Scripts/Events/FirstQueenConversation.cs
+++ Assets/Scripts/Events/FirstQueenConversation.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FirstQueenConversation : MonoBehaviour
 {
-    public int queensHappiness;
+	[SerializeField] public TextMeshProUGUI debugText;
+
+	public int queensHappiness;
 
 	private void Awake()
 	{
 		queensHappiness = 0;
+		debugText.text = queensHappiness.ToString();
 	}
 
-	public void ChangeHappiness(int amount) { queensHappiness += amount; }
+	public void ChangeHappiness(int amount) 
+	{ 
+		queensHappiness += amount; 
+		debugText.text = queensHappiness.ToString(); 
+	}
 }
