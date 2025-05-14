057b0c85fa421550c90f6c90ddd8d2cfcb97431f
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2025-05-14 16:15:10 +0200

    Update DialogueTextNode.cs

--- Assets/Custom Dialogue System/Scripts/Node Types/DialogueTextNode.cs
+++ Assets/Custom Dialogue System/Scripts/Node Types/DialogueTextNode.cs
@@ -6,6 +6,7 @@ public class DialogueTextNode : DialogueNode
 {
 	public override DialogueNodeType type { get { return DialogueNodeType.TEXT; } }
 
+	[TextArea]
 	[SerializeField] string text;
 
     [SerializeField] DialogueNode next;
